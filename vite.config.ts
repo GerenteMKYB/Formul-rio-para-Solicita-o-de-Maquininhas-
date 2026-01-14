bbc0ef19db4e64d7c552e559a957f6aeb7a45c46
GerenteMKYB <gerente@makeyourbank.com.br>
2026-01-14 16:16:13 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/FormularioMKY/",
+  base: "./",
   plugins: [
     react(),
     // Dev tools do Chef (somente em development)
